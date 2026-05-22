b561b2ada9b52d2c5b0aeab573f87dd7915fbb89
Anuj Meena Sharma <anuj.shar2k8@gmail.com>
2026-05-22 18:36:53 +0530

    done

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,7 @@ module.exports = {
       cwd: __dirname,
       instances: 1,
       exec_mode: 'fork',
+      env_file: '.env',
       env: {
         PORT: 3000,
         NODE_ENV: 'production'
@@ -18,6 +19,7 @@ module.exports = {
       cwd: require('path').join(__dirname, 'shopify-express'),
       instances: 1,
       exec_mode: 'fork',
+      env_file: require('path').join(__dirname, '.env'),
       env: {
         PORT: 3001,
         EXPRESS_PORT: 3001,
